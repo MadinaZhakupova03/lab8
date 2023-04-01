b7f4dbc5405d6a9741c7f335cfc54c1826c21e96
Madina <junmadina@mail.ru>
2023-04-01 09:41:29 +0600

    second commit

--- lab8fun.py
+++ lab8fun.py
@@ -1,7 +1,13 @@
+import re
+
 name = input("Введите ваше имя: ")
-if name:
-    print("Привет, " + name + "!")
+
+if name and re.match("^[a-zA-Z]+$", name):
+    if name.istitle():
+        print("Привет, " + name + "!")
+    else:
+        print("Пожалуйста, введите корректное имя.")
 else:
-    print("Пожалуйста, введите ваше имя.")
+    print("Пожалуйста, введите корректное имя.")
 
 print("Спасибо за участие!")
